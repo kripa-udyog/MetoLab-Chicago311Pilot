c45c9b99e60b899aa0a37521ce3ab7428372a335
KR <kripashankar@gmail.com>
2014-10-09 12:35:46 -0500

    trial1

---
+++ Data loader.R
@@ -0,0 +1,14 @@
+# data loader
+
+# datFOIA=read.csv("FOIA_Request_Log.csv")
+# datAbandoned_Vehicles=read.csv("Abandoned_Vehicles.csv")
+# datGarbage_Carts=read.csv("Garbage_Carts.csv")
+# datGraffiti_Removal=read.csv("Graffiti_Removal.csv")
+# datPerfMetrics311CallCenter=read.csv("Performance_Metrics_-_311_Call_Center.csv")
+# datPot_Holes_Reported=read.csv("Pot_Holes_Reported.csv")
+# datSanitation_Code_Complaints=read.csv("Sanitation_Code_Complaints.csv")
+# datStreet_Lights_All_Out=read.csv("Street_Lights_-_All_Out.csv")
+# datStreet_Lights_One_Out=read.csv("Street_Lights_-_One_Out.csv")
+ datTree_Debris=read.csv("Tree_Debris.csv")
+# datTree_Trims=read.csv("Tree_Trims.csv")
+# datVacant_and_Abandoned_Buildings_Reported=read.csv("Vacant_and_Abandoned_Buildings_Reported.csv")
